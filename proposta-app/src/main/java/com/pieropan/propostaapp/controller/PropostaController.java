f0b00c82f8600c4c802a859194f9a38ec3b7682b
Carlos Henrique <ichfs9108@gmail.com>
2024-04-16 02:01:59 -0300

    feat: Criando nossa repository - Interagindo com banco de dados

--- proposta-app/src/main/java/com/pieropan/propostaapp/controller/PropostaController.java
+++ proposta-app/src/main/java/com/pieropan/propostaapp/controller/PropostaController.java
@@ -3,12 +3,14 @@ package com.pieropan.propostaapp.controller;
 import com.pieropan.propostaapp.dto.PropostaRequestDto;
 import com.pieropan.propostaapp.dto.PropostaResponseDto;
 import com.pieropan.propostaapp.service.PropostaService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/proposta")
 public class PropostaController {
@@ -21,3 +23,4 @@ public class PropostaController {
         return ResponseEntity.ok(response);
     }
 }
+
--- proposta-app/src/main/java/com/pieropan/propostaapp/service/PropostaService.java
+++ proposta-app/src/main/java/com/pieropan/propostaapp/service/PropostaService.java
@@ -2,10 +2,19 @@ package com.pieropan.propostaapp.service;
 
 import com.pieropan.propostaapp.dto.PropostaRequestDto;
 import com.pieropan.propostaapp.dto.PropostaResponseDto;
+import com.pieropan.propostaapp.entity.Proposta;
+import com.pieropan.propostaapp.repository.PropostaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class PropostaService {
 
+    private PropostaRepository propostaRepository;;
+
     public PropostaResponseDto criar(PropostaRequestDto requestDto) {
+        propostaRepository.save(new Proposta());
         return null;
     }
 }
