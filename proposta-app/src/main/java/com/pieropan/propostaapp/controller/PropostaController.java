e0de846c90fc1756f3388926d7513b400344d63a
Carlos Henrique <ichfs9108@gmail.com>
2024-04-16 01:24:51 -0300

    feat: Criando nossa service

--- proposta-app/src/main/java/com/pieropan/propostaapp/controller/PropostaController.java
+++ proposta-app/src/main/java/com/pieropan/propostaapp/controller/PropostaController.java
@@ -2,6 +2,7 @@ package com.pieropan.propostaapp.controller;
 
 import com.pieropan.propostaapp.dto.PropostaRequestDto;
 import com.pieropan.propostaapp.dto.PropostaResponseDto;
+import com.pieropan.propostaapp.service.PropostaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/proposta")
 public class PropostaController {
 
+    private PropostaService propostaService;
+
     @PostMapping
     public ResponseEntity<PropostaResponseDto> criar(@RequestBody PropostaRequestDto requestDto) {
-        return null;
+        PropostaResponseDto response = propostaService.criar(requestDto);
+        return ResponseEntity.ok(response);
     }
 }
---
+++ proposta-app/src/main/java/com/pieropan/propostaapp/service/PropostaService.java
@@ -0,0 +1,11 @@
+package com.pieropan.propostaapp.service;
+
+import com.pieropan.propostaapp.dto.PropostaRequestDto;
+import com.pieropan.propostaapp.dto.PropostaResponseDto;
+
+public class PropostaService {
+
+    public PropostaResponseDto criar(PropostaRequestDto requestDto) {
+        return null;
+    }
+}
