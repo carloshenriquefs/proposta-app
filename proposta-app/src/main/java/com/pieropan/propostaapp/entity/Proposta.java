ffc3f97485d67ef06eb737439e4ce3a3a4d77672
Carlos Henrique <ichfs9108@gmail.com>
2024-04-16 01:03:14 -0300

    feat: Adicionando anotacoes do JPA

--- proposta-app/src/main/java/com/pieropan/propostaapp/entity/Proposta.java
+++ proposta-app/src/main/java/com/pieropan/propostaapp/entity/Proposta.java
@@ -3,11 +3,13 @@ package com.pieropan.propostaapp.entity;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "tb_proposta")
 public class Proposta {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "valor")
     private Double valorSolicitado;
     private int prazoPagamento;
     private Boolean aprovada;
