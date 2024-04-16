942fe012d6bd783a2f869fd41251cceaa606eecd
Carlos Henrique <ichfs9108@gmail.com>
2024-04-16 01:15:12 -0300

    feat: Mapeando os nosso DTOs

--- proposta-app/src/main/java/com/pieropan/propostaapp/dto/PropostaRequestDto.java
+++ proposta-app/src/main/java/com/pieropan/propostaapp/dto/PropostaRequestDto.java
@@ -1,4 +1,28 @@
 package com.pieropan.propostaapp.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PropostaRequestDto {
+
+    private String nome;
+
+    private String sobrenome;
+
+    private String telefone;
+
+    private String cpf;
+
+    private Double renda;
+
+    private Double valorSolicitado;
+
+    private int prazoPagamento;
 }
--- proposta-app/src/main/java/com/pieropan/propostaapp/dto/PropostaResponseDto.java
+++ proposta-app/src/main/java/com/pieropan/propostaapp/dto/PropostaResponseDto.java
@@ -1,4 +1,33 @@
 package com.pieropan.propostaapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PropostaResponseDto {
+
+    private Long id;
+
+    private String nome;
+
+    private String sobrenome;
+
+    private String telefone;
+
+    private String cpf;
+
+    private Double renda;
+
+    private Double valorSolicitado;
+
+    private int prazoPagamento;
+
+    private Boolean aprovado;
+
+    private String observacao;
 }
