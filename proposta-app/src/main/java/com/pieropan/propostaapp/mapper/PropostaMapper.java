a4f70a20b780ed721040a729b970a63cb5e1d87b
Carlos Henrique <ichfs9108@gmail.com>
2024-04-17 16:54:35 -0300

    fix: Ajuste no valor solicitado no MapStruct

--- proposta-app/src/main/java/com/pieropan/propostaapp/mapper/PropostaMapper.java
+++ proposta-app/src/main/java/com/pieropan/propostaapp/mapper/PropostaMapper.java
@@ -6,6 +6,8 @@ import com.pieropan.propostaapp.entity.Proposta;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+
+import java.text.NumberFormat;
 import java.util.List;
 
 @Mapper
@@ -29,7 +31,12 @@ public interface PropostaMapper {
     @Mapping(target = "telefone", source = "usuario.telefone")
     @Mapping(target = "cpf", source = "usuario.cpf")
     @Mapping(target = "renda", source = "usuario.renda")
+    @Mapping(target = "valorSolicitadoFmt", expression = "java(setValorSolicitadoFmt(proposta))")
     PropostaResponseDto convertEntityToDto(Proposta proposta);
 
     List<PropostaResponseDto> convertListEntityToListDto(Iterable<Proposta> propostas);
+
+    default String setValorSolicitadoFmt(Proposta proposta) {
+        return NumberFormat.getCurrencyInstance().format(proposta.getValorSolicitado());
+    }
 }
